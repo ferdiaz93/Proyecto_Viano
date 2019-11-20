5425ec7fe086d4d297a696023c57c8367f40b7ea
ferdiaz93 <fernandodiaz1209@gmail.com>
2019-11-20 01:59:09 -0300

    certificados agregados en RUMP

--- scripts/script-idioma-body-rump.js
+++ scripts/script-idioma-body-rump.js
@@ -32,3 +32,30 @@ function cambiarIdiomaBodyRump(idioma) {
             BODY_PARRAFO.innerText = "Viano, Medida de capacidad se encuentra inscripto como reparador de surtidores y como fabricante y reparador de medidas de capacidad bajo el número de R.U.M.P. RL-2018 de acuerdo a los requisitos establecidos por el Ministerio de Producción de la República Argentina.  ";
     }
 }
+
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img1 = document.getElementById("certificado1");
+var img2 = document.getElementById("certificado2");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img1.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+img2.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
