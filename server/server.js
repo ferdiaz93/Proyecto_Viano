b0e4460770b5c4d517c652a3d65d46872d431a3f
MrPlastiTerceros <alant.escudero@outlook.com.ar>
2020-02-01 21:21:09 -0300

    backend added

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
-var express = requiere('express');
+const express = require('express');
+const app = express();
+const path = require("path");
+const fs = require("fs");
+const HomeController = require("./controllers/homeController");
+const AdminController = require("./controllers/adminController");
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", HomeController.getHome);
+app.get("/adminlogin", AdminController.getAdmin);
+app.get("/admintest", AdminController.getAdminTest);
+
+app.listen(5500, function(){
+    console.log("Puerto 5500 habilitado");
+});
