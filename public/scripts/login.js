8e6e7d0e150fcd83b86d667cdfa271b528b4f914
ferdiaz93 <fernandodiaz1209@gmail.com>
2020-03-01 20:46:53 -0300

    fixing weas

---
+++ public/scripts/login.js
@@ -0,0 +1,12 @@
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function(evn) {
+    evn.preventDefault();
+    let name = document.getElementById("inputName").value;
+    let pass = document.getElementById("inputPassword").value;
+    connect(name, pass);
+});
+
+function connect(qname, qpass) {
+    fetch(`http://localhost:5500/admintest?user=${qname}&password=${qpass}`);
+}
